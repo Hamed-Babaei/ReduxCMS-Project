d90c0a4644f2220eb3d99773687eb28eaf45a8cb
Hamed Babaei <dev.hamedbabaei@gmail.com>
2023-11-27 16:38:50 +0330

    Create Routes

--- reduxcms-project/src/App.jsx
+++ reduxcms-project/src/App.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
+
+import "./App.css";
 
 export default function App() {
-  return <div></div>;
+  const router = useRoutes(routes);
+
+  return (
+    <>
+      <Header />
+      <div class="container px-0">
+        <main class="main">
+          <div class="row justify-content-between mx-0">
+            <Sidebar />
+
+            {router}
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
